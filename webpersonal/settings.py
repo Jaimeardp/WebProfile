ddef9e3d9bfed0013fabbff80dde0299df26f627
Jaimeardp <jaimeardp@gmail.com>
2018-05-13 22:27:12 +0000

    fixed deploy app

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -125,18 +125,4 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Subir proyecto a heroku
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
